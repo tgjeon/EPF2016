78b1f9015582015980e15e8d9dfe842612c9e3d5
Taegyun Jeon <tgjeon@gist.ac.kr>
2016-05-04 22:46:42 +0900

    Update epf.py

--- epf.py
+++ epf.py
@@ -9,10 +9,38 @@ import pandas as pd
 import numpy as np
 
 
+def evaluation(prediction, label):
+    return 0
+
+# Parameters
+DATA_FOR_EXPERIMENT = 24 * 478  # Competition schedule until 2016-04-22
+PREDICTION_HOURS = 24 * 1       # D+1
+TRAINING_HOURS = 24 * 365       # (2015-01-01:2015-12-31)
+
+# Read hourly prices from EPF2016 dataset (2015-01-01:2016-04-27)
 dateparse = lambda dates: pd.datetime.strptime(dates, '%d/%m/%Y %H:%M')
 data = pd.read_csv("./input/ElectricityPrice/RealMarketPriceDataPT.csv", 
                    parse_dates={'timeline': ['date', '(UTC)']}, 
                    index_col='timeline', date_parser=dateparse)
                    
                    
+# Cut data until 2016-04-22
+data = data[:DATA_FOR_EXPERIMENT]
                    
+# Make label for future 5-days                    
+labels = data[PREDICTION_HOURS:]#.values                  
+data = data[:-PREDICTION_HOURS]#.values
+
+# Split data into train and test data
+train_data = data[:TRAINING_HOURS]
+train_label = labels[:TRAINING_HOURS]
+
+test_data = data[TRAINING_HOURS:]
+test_label = labels[TRAINING_HOURS:]
+
+
+
+# Comparison groups
+# Linear models: ARMA, ARIMA, Kalman filters
+# Nonlinear models: NN, SVM, Fuzzy system, Bayesian estimators
+
