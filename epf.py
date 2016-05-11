101aa766c7f6573fe3a0375d177c79d7fbed34ea
Taegyun Jeon <tgjeon@gist.ac.kr>
2016-05-11 22:44:08 +0900

    Update epf.py
    DataPreparation

--- epf.py
+++ epf.py
@@ -11,10 +11,13 @@ import numpy as np
 
 def evaluation(prediction, label):
     return 0
+    
+def dataPreparation(data):
+    return 0
 
 # Parameters
 DATA_FOR_EXPERIMENT = 24 * 478  # Competition schedule until 2016-04-22
-PREDICTION_HOURS = 24 * 1       # D+1
+PREDICTION_HOURS = 1            # h+1
 TRAINING_HOURS = 24 * 365       # (2015-01-01:2015-12-31)
 MOVING_WINDOWS_HOURS = 24 * 30  # Moving window with 30 days
 
@@ -29,8 +32,8 @@ data = pd.read_csv("./input/ElectricityPrice/RealMarketPriceDataPT.csv",
 data = data[:DATA_FOR_EXPERIMENT]
                    
 # Make label for future 5-days                    
-labels = data[PREDICTION_HOURS:]#.values                  
-data = data[:-PREDICTION_HOURS]#.values
+labels = data[PREDICTION_HOURS:].values
+data = data[:-PREDICTION_HOURS].values
 
 # Split data into train and test data
 train_data = data[:TRAINING_HOURS]
