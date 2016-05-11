7f66e3da80cad9080c0d1e3e7a8959add828bb94
Taegyun Jeon <tgjeon@gist.ac.kr>
2016-05-11 22:54:19 +0900

    Update epf.py
    Data preparation works

--- epf.py
+++ epf.py
@@ -13,7 +13,23 @@ def evaluation(prediction, label):
     return 0
     
 def dataPreparation(data):
-    return 0
+    # Cut data until 2016-04-22
+    data = data[:DATA_FOR_EXPERIMENT]
+                       
+    # Make label for future 5-days                    
+    labels = data[PREDICTION_HOURS:].values
+    data = data[:-PREDICTION_HOURS].values
+    
+    # Split data into train and test data
+    train_data = data[:TRAINING_HOURS]
+    train_label = labels[:TRAINING_HOURS]
+    
+    test_data = data[TRAINING_HOURS:]
+    test_label = labels[TRAINING_HOURS:]
+    
+    return train_data, train_label, test_data, test_label
+
+
 
 # Parameters
 DATA_FOR_EXPERIMENT = 24 * 478  # Competition schedule until 2016-04-22
@@ -21,26 +37,17 @@ PREDICTION_HOURS = 1            # h+1
 TRAINING_HOURS = 24 * 365       # (2015-01-01:2015-12-31)
 MOVING_WINDOWS_HOURS = 24 * 30  # Moving window with 30 days
 
+
 # Read hourly prices from EPF2016 dataset (2015-01-01:2016-04-27)
 dateparse = lambda dates: pd.datetime.strptime(dates, '%d/%m/%Y %H:%M')
-data = pd.read_csv("./input/ElectricityPrice/RealMarketPriceDataPT.csv", 
+rawdata = pd.read_csv("./input/ElectricityPrice/RealMarketPriceDataPT.csv", 
                    parse_dates={'timeline': ['date', '(UTC)']}, 
                    index_col='timeline', date_parser=dateparse)
                    
-                   
-# Cut data until 2016-04-22
-data = data[:DATA_FOR_EXPERIMENT]
-                   
-# Make label for future 5-days                    
-labels = data[PREDICTION_HOURS:].values
-data = data[:-PREDICTION_HOURS].values
 
-# Split data into train and test data
-train_data = data[:TRAINING_HOURS]
-train_label = labels[:TRAINING_HOURS]
+trainX, trainY, testX, testY = dataPreparation(rawdata)
+                   
 
-test_data = data[TRAINING_HOURS:]
-test_label = labels[TRAINING_HOURS:]
 
 
 
